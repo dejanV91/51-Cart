3ebc1dec221d35276bd6f5b37d2a4a37c93de3ba
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-01 17:03:28 +0100

    initial commit

--- src/shared/reducer.js
+++ src/shared/reducer.js
@@ -18,7 +18,15 @@ const reducer = (state, action) => {
     });
     return { ...state, cart: tempCart };
   }
-
+  if (action.type === "DECREASE") {
+    const tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount - 1 };
+      }
+      return { ...cartItem };
+    });
+    return { ...state, cart: tempCart };
+  }
   return state;
 };
 
