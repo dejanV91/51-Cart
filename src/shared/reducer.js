4d258383d320559e695658a24307a80a0c1ca69f
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-02 15:05:18 +0100

    initial commit

--- src/shared/reducer.js
+++ src/shared/reducer.js
@@ -29,6 +29,18 @@ const reducer = (state, action) => {
       .filter((cartItem) => cartItem.amount !== 0);
     return { ...state, cart: tempCart };
   }
+
+  if (action.type === "GET_TOTAL") {
+    const totalAmount = state.cart.reduce((container, iterator) => {
+      return container + iterator.amount;
+    }, 0);
+    const totalPrice = state.cart.reduce((container, iterator) => {
+      return container + iterator.amount * iterator.price;
+    }, 0);
+
+    return { ...state, amount: totalAmount, total: totalPrice.toFixed(2) };
+  }
+
   return state;
 };
 
